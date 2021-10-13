794deda5666e56b1c5eecce681c5ac59accb3897
Tim <tmetcalfe89@gmail.com>
2021-10-12 22:27:33 -0700

    created basic student card

--- src/components/StudentCard.js
+++ src/components/StudentCard.js
@@ -1,7 +1,23 @@
 import React from "react";
 
-function StudentCard({ student }) {
-  return <div></div>;
+function StudentCard({
+  student: { pic, firstName, lastName, email, company, skill, grades },
+}) {
+  const average = (...arr) =>
+    arr.map((e) => +e).reduce((acc, e) => acc + e, 0) / arr.length;
+
+  return (
+    <div>
+      <img src={pic} alt="Profile" />
+      <h1>
+        {firstName} {lastName}
+      </h1>
+      <div>Email: {email}</div>
+      <div>Company: {company}</div>
+      <div>Skill: {skill}</div>
+      <div>Average: {average(...grades)}%</div>
+    </div>
+  );
 }
 
 export default StudentCard;
