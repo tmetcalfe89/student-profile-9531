94d41fe4288ff997a5ed694a49074d3c24e19593
Tim <tmetcalfe89@gmail.com>
2021-10-12 23:18:51 -0700

    added student card css and wrapped up css

--- src/components/StudentCard.js
+++ src/components/StudentCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./StudentCard.css";
 
 function StudentCard({
   student: { pic, firstName, lastName, email, company, skill, grades },
@@ -7,15 +8,21 @@ function StudentCard({
     arr.map((e) => +e).reduce((acc, e) => acc + e, 0) / arr.length;
 
   return (
-    <div>
-      <img src={pic} alt="Profile" />
-      <h1>
-        {firstName} {lastName}
-      </h1>
-      <div>Email: {email}</div>
-      <div>Company: {company}</div>
-      <div>Skill: {skill}</div>
-      <div>Average: {average(...grades)}%</div>
+    <div className="student-card">
+      <div className="profile-pic">
+        <img src={pic} alt="Profile" />
+      </div>
+      <div className="data">
+        <h1>
+          {firstName} {lastName}
+        </h1>
+        <div className="details">
+          <div>Email: {email}</div>
+          <div>Company: {company}</div>
+          <div>Skill: {skill}</div>
+          <div>Average: {average(...grades)}%</div>
+        </div>
+      </div>
     </div>
   );
 }
