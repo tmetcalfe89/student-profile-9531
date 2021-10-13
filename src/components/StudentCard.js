0b25e809b237fb447656030e8885b590065889eb
Tim <tmetcalfe89@gmail.com>
2021-10-13 00:09:01 -0700

    added tag functionality

--- src/components/StudentCard.js
+++ src/components/StudentCard.js
@@ -1,16 +1,27 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import TestScores from "./TestScores";
+import TagList from "./TagList";
 import "./StudentCard.css";
 
 function StudentCard({
   student: { pic, firstName, lastName, email, company, skill, grades },
 }) {
+  const tagInputRef = useRef(null);
+
   const [expanded, setExpanded] = useState(false);
+  const [tags, setTags] = useState([]);
+  const [tagInput, setTagInput] = useState("");
 
   const average = (...arr) =>
     arr.map((e) => +e).reduce((acc, e) => acc + e, 0) / arr.length;
 
   const toggleExpanded = () => setExpanded(!expanded);
+
+  const addTag = (e) => {
+    e.preventDefault();
+    setTags([...tags, tagInputRef.current.value]);
+    setTagInput("");
+  };
 
   return (
     <div className="student-card">
@@ -26,6 +37,15 @@ function StudentCard({
           <div>Company: {company}</div>
           <div>Skill: {skill}</div>
           <div>Average: {average(...grades)}%</div>
+          {tags.length > 0 && <TagList tags={tags} />}
+          <form onSubmit={addTag}>
+            <input
+              placeholder="Add a tag"
+              value={tagInput}
+              onChange={(e) => setTagInput(e.target.value)}
+              ref={tagInputRef}
+            />
+          </form>
           {expanded && <TestScores scores={grades} />}
         </div>
       </div>
---
+++ src/components/TagList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Tag from "./Tag";
+
+function TagList({ tags }) {
+  return (
+    <div>
+      {tags.map((tag) => (
+        <Tag tag={tag} />
+      ))}
+    </div>
+  );
+}
+
+export default TagList;
