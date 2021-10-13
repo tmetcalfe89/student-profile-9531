6fe817c5c1873ab9005a62d5b6ff05747f94bc7d
Tim <tmetcalfe89@gmail.com>
2021-10-12 22:20:47 -0700

    added studentList state var and fetched from API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [studentList, setStudentList] = useState([]);
+
+  useEffect(() => {
+    const getStudents = async () => {
+      const studentResponse = await fetch(
+        "https://api.hatchways.io/assessment/students"
+      );
+      const studentData = await studentResponse.json();
+      setStudentList(studentData.students);
+    };
+    getStudents();
+  }, []);
+
   return <div></div>;
 }
 
