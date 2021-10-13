46671873f21c0234cee1e185b033835b1515eadd
Tim <tmetcalfe89@gmail.com>
2021-10-12 23:43:04 -0700

    added name filtering logic

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import StudentCard from "./StudentCard";
 import "./StudentList.css";
 
 function StudentList({ students }) {
+  const [filters, setFilters] = useState({});
+
+  const updateFilter = (event) => {
+    setFilters({ ...filters, [event.target.name]: event.target.value });
+  };
+
+  const getFilteredStudents = () => {
+    return students.filter(
+      ({ firstName, lastName }) =>
+        !filters.name ||
+        `${firstName} ${lastName}`.toLowerCase().includes(filters.name)
+    );
+  };
+
   return (
     <div className="student-list-container">
-      <input placeholder="Search by name" />
+      <input
+        placeholder="Search by name"
+        name="name"
+        value={filters.name}
+        onChange={updateFilter}
+      />
       <div className="student-list">
-        {students.map((student) => (
+        {getFilteredStudents().map((student) => (
           <StudentCard student={student} />
         ))}
       </div>
